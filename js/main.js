d0fd10b92aeb14721bad09f1c9686a1c9843c0fc
miistrass <milenastrass@gmail.com>
2023-03-20 23:45:03 -0300

    criado o arquivo main.js

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const robotron = document.querySelector("#robotron")
+
+robotron.addEventListener("click", () => {
+  console.log("Cliquei no robô")
+})
+
+function dizOi() {
+  console.log("Oi")
+  console.log("Bem-vindo ao Robotron 2000")
+}
+
+dizOi()
